d8ea0c870bbd2faf809cba38835493cffafba7e3
Utkarsh M <utkarshrm568@gmail.com>
2021-09-15 15:39:27 +0530

    Readme Update and name change (#18)

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,6 +10,10 @@ const router = express.Router({ mergeParams: true });
 /* Same management page will be given */
 /* Admin related routes */
 router.route('/users/:userId/type/')
+
+/* req.body should contain the type.
+userId is the username(not the objectId of the user)
+*/
   .put(isLoggedIn,
     isAdmin, admin.changeUserType);
 module.exports = router;
